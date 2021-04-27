e9a155e1f3a6a3d8725107c0f7605baa29de9893
Yasir Sarole <yasirsarole81@gmail.com>
2021-04-28 04:07:13 +0530

    Axios setup finished

--- src/components/GenreDetail/index.js
+++ src/components/GenreDetail/index.js
@@ -1,6 +1,17 @@
 import React from 'react';
+import axios from 'axios';
 
 class GenreDetail extends React.Component {
+    componentDidMount() {
+        axios.get('http://skunkworks.ignitesol.com:8000/books')
+          .then(function (response) {
+            console.log(response);
+          })
+          .catch(function (error) {
+            console.log(error);
+          })
+    }
+
     render() {
         return <div>This is genre detail page</div>
     }
