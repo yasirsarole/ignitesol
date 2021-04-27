907ad96db717dc902e5963bff3b4d5a43ca606e5
Yasir Sarole <yasirsarole81@gmail.com>
2021-04-28 03:37:19 +0530

    Added initial setup

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Gutenberg from './components/App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Gutenberg />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
